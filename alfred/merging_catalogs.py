e67c48deef096ff99b1fc3904e61987fb58bf0b8
keexcell <112121004+keexcell@users.noreply.github.com>
2026-07-28 12:20:16 -0500

    Drafted structure for the merging_catalogs function
    Function to create Euclid and Rubin datasets and register them to the data registry on NERSC.

---
+++ alfred/merging_catalogs.py
@@ -0,0 +1,11 @@
+## function to create euclid + rubin datasets and register to the data registry on NERSC
+
+# first check for sure the data doesn't exist already
+
+# for a given tract number for rubin, we know the coord circle
+
+# astroquery euclid for that circle, will take a long time
+
+# match and merge
+
+# output merged catalog as a MergedData object
