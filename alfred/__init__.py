e70111df9a4fdad65043a1ee45839b5c8d5d1c3c
Kayleigh Excell <kexcell@Kayleighs-MacBook-Pro.local>
2026-07-28 10:35:57 -0500

    Making __init__ and pyproject

---
+++ alfred/__init__.py
@@ -0,0 +1,5 @@
+#from .file.subfile import function
+#allows user to just type from my_package import function
+
+#__all__ = ["main function", "public function", "important function"]
+#controls which functions are imported when a user does from my_package import *
